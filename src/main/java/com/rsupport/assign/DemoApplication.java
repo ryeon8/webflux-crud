b527f8685ace1b9cf454b096693d17cb52f856dd
ryeon <ryeon530@gmail.com>
2024-03-16 19:07:35 +0900

    webflux 설정 추가

--- src/main/java/com/rsupport/assign/DemoApplication.java
+++ src/main/java/com/rsupport/assign/DemoApplication.java
@@ -2,8 +2,10 @@ package com.rsupport.assign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rsupport/assign/noti/web/NotiController.java
+++ src/main/java/com/rsupport/assign/noti/web/NotiController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
 @RestController
 @RequestMapping("/noti")
 public class NotiController {
 
   @GetMapping("/test")
-  public String test() {
-    return "hello, world";
+  public Mono<String> test() {
+    return Mono.just("hello, world");
   }
 
 }
